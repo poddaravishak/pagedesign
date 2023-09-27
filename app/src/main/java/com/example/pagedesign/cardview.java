898ea207745826638954ae6496fa4e708d4781bc
avishak-poddar <avishakpoddar@gmail.com>
2023-09-27 21:28:16 +0600

    page design

---
+++ app/src/main/java/com/example/pagedesign/cardview.java
@@ -0,0 +1,14 @@
+package com.example.pagedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cardview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview);
+    }
+}
